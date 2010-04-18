c6a608a913dbaa88be921ab4ba267704d59e4188
tehfink <djsnickles@yahoo.com>
2010-04-17 20:54:45 -0400

    fixed: "AttributeError: 'module' object has no attribute 'FilerFile'", etc.

--- src/cmsplugin_filer_teaser/cms_plugins.py
+++ src/cmsplugin_filer_teaser/cms_plugins.py
@@ -1,7 +1,7 @@
 from cms.plugin_pool import plugin_pool
 from cms.plugin_base import CMSPluginBase
 from django.utils.translation import ugettext_lazy as _
-import models
+from cmsplugin_filer_teaser import models
 from django.conf import settings
 
 class FilerTeaserPlugin(CMSPluginBase):
